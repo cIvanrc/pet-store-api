1ca4b9aff56e37f9a674572e0100962dab532de6
ivan <c.ivan.felix.r@gmail.com>
2021-09-08 22:45:13 -0500

    added JsonWebToken service

---
+++ app/services/json_web_token.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Handles the #encode and #decode JWT
+class JsonWebToken
+  def self.encode(payload)
+    expiration = 60.minutes.from_now.to_i
+    payload = payload.merge(exp: expiration)
+    JWT.encode(payload, jwt_secret)
+  end
+
+  def self.decode(token)
+    JWT.decode(token, jwt_secret).first
+  end
+
+  def self.jwt_secret
+    ENV.fetch('JWT_SECRET', 'xoo1Eigh')
+  end
+
+  private_class_method :jwt_secret
+end

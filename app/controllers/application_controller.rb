9026a05a6f533b6bcc98a679f5184c7994a42fef
Ivan <c.ivan.felix.r@gmail.com>
2021-09-08 22:54:17 -0500

    add jwt generation (#8)
    * added user spec (but probably users no are going to be needed)
    
    * added JsonWebToken service
    
    * added before_action to authenticate
    
    * added Authentiaction controller, that generates a JWT
    
    * added jwt generation spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :set_default_format
+  before_action :authenticate!
+
+  private
+  def set_default_format
+    request.format = :json
+  end
+
+  def authenticate!
+    JsonWebToken.decode(auth_token)
+  rescue JWT::DecodeError
+    render json: { errors: ['invalid auth token'] }, status: :unauthorized
+  end
+
+  def auth_token
+    @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it {should validate_presence_of(:email)}
+  it {should validate_presence_of(:password)}
+
+  it 'should validate email uniqueness' do
+    user_valid = create(:user, email: 'example_email@domain.com')
+    user_invalid = build(:user, email: 'example_email@domain.com')
+    expect { user_invalid.save! }.to raise_error(
+      ActiveRecord::RecordInvalid
+    )
+  end
 end

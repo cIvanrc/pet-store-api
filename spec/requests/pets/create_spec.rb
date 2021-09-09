13af8f9bba401dface8c5ca661040578784bdc72
ivan <c.ivan.felix.r@gmail.com>
2021-09-09 02:48:58 -0500

    added #create specs

---
+++ spec/requests/pets/create_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'Create a Pet', type: :request do
+  context 'Can create a pet' do
+    let (:pet_params) { { pet: { name: 'new_pet' } } }
+
+    it 'on success' do
+      post api_v1_pets_path, headers: authorization_headers, params: pet_params
+
+      expect(response.body.present?).to be false
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  context 'fails' do
+    it 'without jwt' do
+      post api_v1_pets_path
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'without name param' do
+      post api_v1_pets_path, headers: authorization_headers, params: { pet: {tag: 'tag', name: ''} }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:errors][:name].first).to eq("can't be blank")
+
+    end
+
+    it 'without params' do
+      post api_v1_pets_path, headers: authorization_headers, params: { }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:errors].first).to eq 'param is missing or the value is empty'
+    end
+  end
+end

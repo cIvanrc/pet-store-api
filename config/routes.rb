aa623290a5d958f18d3f9830a0571b9cda258a09
Ivan <c.ivan.felix.r@gmail.com>
2021-09-09 13:36:32 -0500

    get a pet (#11)
    * added pet show route
    
    * added validation for record not found
    
    * added pet#show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'token', to: 'authentication#create'
 
-      resources :pets, only: %i[index create]
+      resources :pets, only: %i[index create show]
     end
   end
 end

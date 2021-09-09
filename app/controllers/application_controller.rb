dd8f012f9d1d0028a8065fd7725022f9a8e18041
ivan <c.ivan.felix.r@gmail.com>
2021-09-09 13:29:05 -0500

    added validation for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error_record_not_found
+
   before_action :set_default_format
   before_action :authenticate!
 
@@ -15,5 +17,9 @@ class ApplicationController < ActionController::API
 
   def auth_token
     @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
+  end
+
+  def error_record_not_found
+    render json: { code: 199, message: 'Unexpected error' }
   end
 end
